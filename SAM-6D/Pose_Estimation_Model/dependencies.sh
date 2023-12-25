1726f8355b96243d47f7d304ea94e4dd540a9e8b
Jiehong <mortimer.jh.lin@gmail.com>
2023-12-25 11:32:07 +0800

    Add files via upload

---
+++ SAM-6D/Pose_Estimation_Model/dependencies.sh
@@ -0,0 +1,14 @@
+
+
+
+pip install timm
+pip install gorilla-core==0.2.6.0
+pip install trimesh==3.22.1
+pip install imgaug
+pip install opencv-python
+pip install gpustat==1.0.0
+pip install einops
+
+cd /model/pointnet2
+python setup.py install
+cd ..
