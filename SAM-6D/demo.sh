5409445e569b1a2707135ef3db8266621b4655b7
foollh <lihualiu.scut@gmail.com>
2024-01-10 08:48:24 +0000

    release a demo

---
+++ SAM-6D/demo.sh
@@ -0,0 +1,14 @@
+# Render CAD templates
+cd Render
+blenderproc run render_single_obj_templates.py --output_dir $OUTPUT_PATH --cad_path $CAD_PATH 
+
+# Run instance segmentation model
+cd ../Instance_Segmentation_Model
+python run_inference_custom.py --output_dir $OUTPUT_PATH --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH
+
+# Run pose estimation model
+export SEG_PATH=$OUTPUT_PATH/sam6d_results/detection_ism.json
+
+cd ../Pose_Estimation_Model
+python run_inference_custom.py --output_dir $OUTPUT_PATH --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH --seg_path $SEG_PATH
+
